823adf0a18cf02a72e3d9719ca7fbddff73df463
Jack Huang <jackhwl@hotmail.com>
2018-10-23 19:57:15 -0400

    explicitly start thread

---
+++ CLR-Threading/multithreading/tpdemo/tpdemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tpdemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("[{0}] Main called", Thread.CurrentThread.ManagedThreadId);
+
+            Thread t = new Thread(SayHello);
+            t.IsBackground = true;
+            t.Start(10);
+            t.Join();
+
+            Console.WriteLine("[{0}] Main done", Thread.CurrentThread.ManagedThreadId);
+            Console.ReadLine();
+        }
+
+        static void SayHello(object arg)
+        {
+            int iterations = (int)arg;
+
+            for (int n = 0; n < iterations; n++)
+            {
+                Console.WriteLine("[{0}] Hello, world {1}! ({2})",
+                    Thread.CurrentThread.ManagedThreadId,
+                    n,
+                    Thread.CurrentThread.IsBackground);
+            }
+        }
+    }
+}
