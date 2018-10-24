e5a0d01da9e3eb9cbd5063bd5434cb97e0e4a90d
Jack Huang <jackhwl@hotmail.com>
2018-10-23 20:39:53 -0400

    with Thread.Sleep

--- CLR-Threading/multithreading/tpdemo/tpdemo/Program.cs
+++ CLR-Threading/multithreading/tpdemo/tpdemo/Program.cs
@@ -2,6 +2,7 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,12 +19,16 @@ namespace tpdemo
                 ThreadPool.QueueUserWorkItem(SayHello, n);
             }
 
+            Thread.Sleep(rng.Next(1500, 2000));
             Console.WriteLine("[{0}] Main done", Thread.CurrentThread.ManagedThreadId);
             Console.ReadLine();
         }
 
+        static Random rng = new Random();
+
         static void SayHello(object arg)
         {
+            Thread.Sleep(rng.Next(250, 500));
             int n = (int) arg;
             Console.WriteLine("[{0}] Hello, world {1}! ({2})",
                 Thread.CurrentThread.ManagedThreadId,
