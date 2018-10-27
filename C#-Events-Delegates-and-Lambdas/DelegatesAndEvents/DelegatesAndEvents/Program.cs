c7e37e81a7fe90c6078875d6157015a6865e415a
Jack Huang <jackhwl@hotmail.com>
2018-10-27 09:45:55 -0400

    Adding Multiple delegates

---
+++ C#-Events-Delegates-and-Lambdas/DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            //del1(5, WorkType.Golf);
+            //del2(10, WorkType.GenerateReports);
+
+            //DoWork(del1);
+
+            //del1 += del2;
+            //del1 += del3;
+
+            del1 += del2 + del3;
+
+            del1(10, WorkType.GoToMeetings);
+
+            Console.ReadLine();
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GenerateReports);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        }
+
+        static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
