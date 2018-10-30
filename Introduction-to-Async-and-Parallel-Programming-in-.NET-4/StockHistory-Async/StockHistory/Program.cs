36c94464985e9826f3bfc2d4f66f1daed4ccad4e
Jack Huang <jackhwl@hotmail.com>
2018-10-30 09:12:29 -0400

    Passing Data and Output Results

--- Introduction-to-Async-and-Parallel-Programming-in-.NET-4/StockHistory-Async/StockHistory/Program.cs
+++ Introduction-to-Async-and-Parallel-Programming-in-.NET-4/StockHistory-Async/StockHistory/Program.cs
@@ -132,38 +132,70 @@ namespace StockHistory
 		        //t_stderr.Wait();
 		        Task[] tasks = {t_min, t_max, t_avg, t_stddev, t_stderr, t_error};
 		        Task.WaitAll(tasks);
-		        lock (Console.Out)
-		        {
-		            Console.WriteLine();
-		            Console.WriteLine("** {0} **", symbol);
-		            Console.WriteLine("   Data source:  '{0}'", data.DataSource);
-		            Console.WriteLine("   Data points:   {0:#,##0}", N);
-		            Console.WriteLine("   Min price:    {0:C}", t_min.Result);
-		            Console.WriteLine("   Max price:    {0:C}", t_max.Result);
-		            Console.WriteLine("   Avg price:    {0:C}", t_avg.Result);
-		            Console.WriteLine("   Std dev/err:   {0:0.000} / {1:0.000}", t_stddev.Result, t_stderr.Result);
-		        }
-		    }
+		        //lock (Console.Out)
+		        //{
+		        //    Console.WriteLine();
+		        //    Console.WriteLine("** {0} **", symbol);
+		        //    Console.WriteLine("   Data source:  '{0}'", data.DataSource);
+		        //    Console.WriteLine("   Data points:   {0:#,##0}", N);
+		        //    Console.WriteLine("   Min price:    {0:C}", t_min.Result);
+		        //    Console.WriteLine("   Max price:    {0:C}", t_max.Result);
+		        //    Console.WriteLine("   Avg price:    {0:C}", t_avg.Result);
+		        //    Console.WriteLine("   Std dev/err:   {0:0.000} / {1:0.000}", t_stddev.Result, t_stderr.Result);
+		        //}
+
+		        decimal min0 = t_min.Result;
+		        decimal max0 = t_max.Result;
+		        decimal avg0 = t_avg.Result;
+		        double stddev0 = t_stddev.Result;
+		        double stderr0 = t_stderr.Result;
+
+		        //
+		        // Output:
+		        //
+		        // NOTE: we build a single output string and then write to the console in one op.
+		        // Otherwise, if we output one value at a time, our output might get intermixed
+		        // with the output from another, parallel task.  Note that Console class is thread-
+		        // safe, so this works as long as we make only a single call for our output.
+		        //
+		        string output = string.Format("\n** {0} **", symbol);
+		        output = string.Format("{0}\n   Data source:  '{1}'", output, data.DataSource);
+		        output = string.Format("{0}\n   Data points:   {1:#,##0}", output, N);
+		        output = string.Format("{0}\n   Min price:    {1:C}", output, min0);
+		        output = string.Format("{0}\n   Max price:    {1:C}", output, max0);
+		        output = string.Format("{0}\n   Avg price:    {1:C}", output, avg0);
+		        output = string.Format("{0}\n   Std dev/err:   {1:0.000} / {2:0.000}", output, stddev0, stderr0);
+
+		        Console.WriteLine(output);		    }
 		    catch (AggregateException ae)
 		    {
-		        lock (Console.Out)
-		        {
-		            Console.WriteLine();
-		            ae = ae.Flatten();
-		            foreach (var ex in ae.InnerExceptions)
-		            {
-		                Console.WriteLine("Tasking Error: {0}", ex.Message);
-		            }
-		        }
+		        //lock (Console.Out)
+		        //{
+		        //    Console.WriteLine();
+		        //    ae = ae.Flatten();
+		        //    foreach (var ex in ae.InnerExceptions)
+		        //    {
+		        //        Console.WriteLine("Tasking Error: {0}", ex.Message);
+		        //    }
+		        //}
+		        string output = string.Format("\n** {0} **", symbol);
+
+		        ae = ae.Flatten();  // could have a tree of exceptions, so flatten first:
+		        foreach (Exception ex in ae.InnerExceptions)
+		            output = string.Format("{0}\nError: {1}", output, ex.Message);
+          
+		        Console.WriteLine(output);
 		    }
 			catch (Exception ex)
 			{
-			    lock (Console.Out)
-			    {
-			        Console.WriteLine();
-			        Console.WriteLine("** {0} **", symbol);
-			        Console.WriteLine("Error: {0}", ex.Message);
-			    }
+			    //lock (Console.Out)
+			    //{
+			    //    Console.WriteLine();
+			    //    Console.WriteLine("** {0} **", symbol);
+			    //    Console.WriteLine("Error: {0}", ex.Message);
+			    //}
+			    string output = string.Format("\n** {0} **\nError: {1}", symbol, ex.Message);
+			    Console.WriteLine(output);
 			}
 		}
 
