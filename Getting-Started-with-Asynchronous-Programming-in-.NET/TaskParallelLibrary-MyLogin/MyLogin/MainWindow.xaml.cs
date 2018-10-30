bd69d204686fa6b804baf194ddb951020e14e820
Jack Huang <jackhwl@hotmail.com>
2018-10-30 19:25:43 -0400

    Task Run and ContinueWith

---
+++ Getting-Started-with-Asynchronous-Programming-in-.NET/TaskParallelLibrary-MyLogin/MyLogin/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyLogin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginButton.IsEnabled = false;
+            var task = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "Login Successful!";
+            });
+
+            task.ContinueWith((t) =>
+            {
+                if (t.IsFaulted)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        LoginButton.Content = "Login failed!";
+                        LoginButton.IsEnabled = true;
+                    });
+                }
+                else
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        LoginButton.Content = t.Result;
+                        LoginButton.IsEnabled = true;
+                    });
+                }
+                
+            });
+        }
+    }
+}
---
+++ Getting-Started-with-Asynchronous-Programming-in-.NET/TaskParallelLibrary-MyLogin/MyLogin/UserControls/BusyIndicator.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MyLogin.UserControls
+{
+    public partial class BusyIndicator : UserControl
+    {
+        public BusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
