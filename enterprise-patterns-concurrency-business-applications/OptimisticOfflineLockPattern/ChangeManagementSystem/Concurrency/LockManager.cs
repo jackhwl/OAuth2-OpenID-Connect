18292eeab29a3e6c22c4c8d195644f62b32f095e
Jack Huang <jackhwl@hotmail.com>
2018-11-06 10:43:36 -0500

    Pessimistic Offline Lock Pattern: Releasing Expired Locks

--- enterprise-patterns-concurrency-business-applications/OptimisticOfflineLockPattern/ChangeManagementSystem/Concurrency/LockManager.cs
+++ enterprise-patterns-concurrency-business-applications/OptimisticOfflineLockPattern/ChangeManagementSystem/Concurrency/LockManager.cs
@@ -7,6 +7,7 @@ namespace ChangeManagementSystem.Concurrency
 {
     public class LockManager
     {
+        private readonly int _lockExpirySeconds = 15;
         private AppDataContext _appDataContext;
 
         public LockManager(AppDataContext appDataContext)
@@ -16,6 +17,7 @@ namespace ChangeManagementSystem.Concurrency
 
         public void AcquireLock(int id, string entityName, string owner)
         {
+            RemoveExpiredLock(id, entityName);
             if (!HasLock(id, entityName, owner))
             {
                 try
@@ -78,5 +80,51 @@ namespace ChangeManagementSystem.Concurrency
                 _appDataContext.SaveChanges();
             }
         }
+
+        // ======= Create this function in DB =========
+        //
+        // CREATE FUNCTION GetLockWithUPDLock(@entityId int, @entityName nvarchar(450))
+        // RETURNS TABLE
+        // AS
+        // RETURN 
+        //      SELECT EntityId, EntityName, AcquiredDateTime, OwnerId
+        //      FROM dbo.Locks WITH (UPDLOCK)
+        //      WHERE EntityId = @entityId AND EntityName = @entityName
+
+        private void RemoveExpiredLock(int id, string entityName)
+        {
+            Lock _lock = _appDataContext.Locks
+                .FromSql("SELECT * FROM dbo.GetLockWithUPDLock({0},{1})", id, entityName)
+                .AsNoTracking()
+                .FirstOrDefault();
+
+            if (_lock != null && _lock.AcquiredDateTime <= DateTime.Now.AddSeconds(-_lockExpirySeconds))
+            {
+                _appDataContext.Locks.Remove(_lock);
+                _appDataContext.SaveChanges();
+            }
+        }
+
+        public void RenewLock(int id, string entityName, string currentUser)
+        {
+            Lock _lock = _appDataContext.Locks
+                .FromSql("SELECT * FROM dbo.GetLockWithUPDLock({0},{1})", id, entityName)
+                .AsNoTracking()
+                .FirstOrDefault();
+            if (_lock != null && _lock.OwnerId == currentUser)
+            {
+                if (_lock.AcquiredDateTime <= DateTime.Now.AddSeconds(-_lockExpirySeconds))
+                {
+                    throw new ConcurrencyException("Lock on Entity has expired. Please restart Business Transaction.");
+                }
+                _lock.AcquiredDateTime = DateTime.Now;
+                _appDataContext.SaveChanges();
+            }
+            else
+            {
+                throw new ConcurrencyException("Lock not found for user and Entity");
+            }
+        }
+
     }
 }
