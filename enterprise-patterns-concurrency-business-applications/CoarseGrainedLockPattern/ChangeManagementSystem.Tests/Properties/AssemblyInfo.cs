a9682d495df72f797a071590b15301fcd406aa5d
Jack Huang <jackhwl@hotmail.com>
2018-11-07 07:19:47 -0500

    Coarse-grained Lock Pattern initial

---
+++ enterprise-patterns-concurrency-business-applications/CoarseGrainedLockPattern/ChangeManagementSystem.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChangeManagementSystem.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChangeManagementSystem.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8ac13c0-401e-454d-9e71-6b99cd47732d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
