8a652f3cfcd7d6aa2a10110be64c69967231d150
Jack Huang <jackhwl@hotmail.com>
2018-11-14 07:23:19 -0500

    Adding a User Interface for IdentityServer4

--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/Marvin.IDP/Startup.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/Marvin.IDP/Startup.cs
@@ -15,6 +15,8 @@ namespace Marvin.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(Config.GetUsers())
@@ -31,6 +33,9 @@ namespace Marvin.IDP
             }
 
             app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
