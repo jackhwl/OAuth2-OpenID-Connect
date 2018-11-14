547a3dec0f9635a36c864d67c1569fbbc58d85d8
Jack Huang <jackhwl@hotmail.com>
2018-11-13 22:21:44 -0500

    add Marvin.IDP

---
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/Marvin.IDP/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "Frank",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Claire",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                }
+            };
+        }
+    }
+}
