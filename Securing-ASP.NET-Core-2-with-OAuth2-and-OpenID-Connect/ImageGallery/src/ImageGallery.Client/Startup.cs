36aaa1780b4e9649e0c55bb8eefb4f46849d4e31
Jack Huang <jackhwl@hotmail.com>
2018-11-15 07:30:31 -0500

    Securing Your Web Application: Returning Addtional Claims

--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Startup.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Startup.cs
@@ -47,6 +47,7 @@ namespace ImageGallery.Client
                     options.Scope.Add("profile");
                     options.SaveTokens = true;
                     options.ClientSecret = "secret";
+                    options.GetClaimsFromUserInfoEndpoint = true;
                 });
         }
 
