b037719b354a95bc79c5a0e649c0ba23288333e4
Jack Huang <jackhwl@hotmail.com>
2018-11-15 11:25:51 -0500

    Working with Claims: Role-based Authorization: Ensuring the Role Is Included

--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/Marvin.IDP/Config.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/Marvin.IDP/Config.cs
@@ -28,6 +28,7 @@ namespace Marvin.IDP
                         new Claim("given_name", "Frank"),
                         new Claim("family_name", "Underwood"),
                         new Claim("address", "Main Road 1"),
+                        new Claim("role", "FreeUser"),
                     }
                 },
                 new TestUser
@@ -41,6 +42,7 @@ namespace Marvin.IDP
                         new Claim("given_name", "Claire"),
                         new Claim("family_name", "Underwood"),
                         new Claim("address", "Big Street 2"),
+                        new Claim("role", "PayingUser"),
                     }
                 }
             };
@@ -53,7 +55,9 @@ namespace Marvin.IDP
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                new IdentityResources.Address()
+                new IdentityResources.Address(),
+                new IdentityResource(
+                    "roles", "Your role(s)", new List<string>() {"role"})
             };
         }
 
@@ -78,7 +82,8 @@ namespace Marvin.IDP
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
-                        IdentityServerConstants.StandardScopes.Address
+                        IdentityServerConstants.StandardScopes.Address,
+                        "roles"
 
                     },
                     ClientSecrets =
--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Startup.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Startup.cs
@@ -49,6 +49,7 @@ namespace ImageGallery.Client
                     options.Scope.Add("openid");
                     options.Scope.Add("profile");
                     options.Scope.Add("address");
+                    options.Scope.Add("roles");
                     options.SaveTokens = true;
                     options.ClientSecret = "secret";
                     options.GetClaimsFromUserInfoEndpoint = true;
@@ -56,6 +57,7 @@ namespace ImageGallery.Client
                     options.ClaimActions.DeleteClaim("sid");
                     options.ClaimActions.DeleteClaim("idp");
                     //options.ClaimActions.DeleteClaim("address");
+                    options.ClaimActions.MapUniqueJsonKey("role", "role");
                 });
         }
 
