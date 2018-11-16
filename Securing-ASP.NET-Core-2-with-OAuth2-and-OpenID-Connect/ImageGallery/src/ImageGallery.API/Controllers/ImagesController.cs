ce31797dd7a38977ca603f4be0b197040cc6eacd
Jack Huang <jackhwl@hotmail.com>
2018-11-16 09:15:41 -0500

    Securing Your API: Using Access Token Claims When Getting a Resource Collection

--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 
 namespace ImageGallery.API.Controllers
@@ -28,8 +29,10 @@ namespace ImageGallery.API.Controllers
         [HttpGet()]
         public IActionResult GetImages()
         {
+            var ownerId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+
             // get from repo
-            var imagesFromRepo = _galleryRepository.GetImages();
+            var imagesFromRepo = _galleryRepository.GetImages(ownerId);
 
             // map to model
             var imagesToReturn = Mapper.Map<IEnumerable<Model.Image>>(imagesFromRepo);
