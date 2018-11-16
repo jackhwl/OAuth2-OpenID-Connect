94a4638421f1e9374e19a7f5de47a97dd2fbae6e
Jack Huang <jackhwl@hotmail.com>
2018-11-16 09:03:51 -0500

    Securing Your API: Showing an Access Denied Page

--- Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ Securing-ASP.NET-Core-2-with-OAuth2-and-OpenID-Connect/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel.Client;
@@ -44,7 +45,12 @@ namespace ImageGallery.Client.Controllers
                     JsonConvert.DeserializeObject<IList<Image>>(imagesAsString).ToList());
 
                 return View(galleryIndexViewModel);
-            }          
+            }
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("AccessDenied", "Authorization");
+            }
 
             throw new Exception($"A problem happened while calling the API: {response.ReasonPhrase}");
         }
