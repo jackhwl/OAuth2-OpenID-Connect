1727e6dde8e688645fe931208bfd44ddbb68006a
Jack Huang <jackhwl@hotmail.com>
2018-12-16 09:18:58 -0500

    Hosting in Console App

---
+++ WCF-Jumpstart/ZzaApp/Zza.SelfHost/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Services;
+
+namespace Zza.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(ZzaService));
+                host.Open();
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
