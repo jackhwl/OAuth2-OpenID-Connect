ede381decd5adc3c713125fdcd154664a3cf4d58
Jack Huang <jackhwl@hotmail.com>
2018-12-17 20:47:51 -0500

    Creating a Custom User Store

---
+++ Securing-ASP.NET-Core-with-OAuth2-and-OpenID-Connect/src/Marvin.IDP/Entities/MarvinUserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marvin.IDP.Entities
+{
+    public class MarvinUserContext : DbContext
+    {
+        public MarvinUserContext(DbContextOptions<MarvinUserContext> options)
+           : base(options)
+        {
+           
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Securing-ASP.NET-Core-with-OAuth2-and-OpenID-Connect/src/Marvin.IDP/Startup.cs
+++ Securing-ASP.NET-Core-with-OAuth2-and-OpenID-Connect/src/Marvin.IDP/Startup.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Marvin.IDP.Entities;
+using Marvin.IDP.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,10 +16,19 @@ namespace Marvin.IDP
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration["connectionStrings:marvinUserDBConnectionString"];
+            services.AddDbContext<MarvinUserContext>(o => o.UseSqlServer(connectionString));
+            services.AddScoped<IMarvinUserRepository, MarvinUserRepository>();
+
             services.AddMvc();
 
             services.AddIdentityServer()
@@ -28,7 +41,7 @@ namespace Marvin.IDP
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, MarvinUserContext marvinUserContext)
         {
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
@@ -37,6 +50,9 @@ namespace Marvin.IDP
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            marvinUserContext.Database.Migrate();
+            marvinUserContext.EnsureSeedDataForContext();
 
             app.UseIdentityServer();
             app.UseStaticFiles();
