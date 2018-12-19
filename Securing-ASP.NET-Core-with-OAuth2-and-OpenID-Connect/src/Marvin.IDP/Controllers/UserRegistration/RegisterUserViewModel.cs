2968b9db5ef316d224b497ae26955b89fde914e0
Jack Huang <jackhwl@hotmail.com>
2018-12-19 11:49:35 -0500

    User Registration

---
+++ Securing-ASP.NET-Core-with-OAuth2-and-OpenID-Connect/src/Marvin.IDP/Controllers/UserRegistration/RegisterUserViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Marvin.IDP.Controllers.UserRegistration
+{
+    public class RegisterUserViewModel
+    {
+        // credentials
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [MaxLength(100)]
+        public string Password { get; set; }
+
+        // claims
+        [Required]
+        [MaxLength(100)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string Country { get; set; }
+
+        public SelectList CountryCodes { get; set; } = new SelectList(new []
+        {
+            new {Id="BE", Value="Belgium"},
+            new {Id="US", Value="United States of America"},
+            new {Id="IN", Value="India"}}, "Id", "Value"
+        );
+
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Securing-ASP.NET-Core-with-OAuth2-and-OpenID-Connect/src/Marvin.IDP/Controllers/UserRegistration/UserRegistrationController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Marvin.IDP.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Marvin.IDP.Controllers.UserRegistration
+{
+    public class UserRegistrationController : Controller
+    {
+        private readonly IMarvinUserRepository _marvinUserRepository;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public UserRegistrationController(IMarvinUserRepository marvinUserRepository, IIdentityServerInteractionService interaction)
+        {
+            _marvinUserRepository = marvinUserRepository;
+            _interaction = interaction;
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser(string returnUrl)
+        {
+            var vm = new RegisterUserViewModel()
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // create user + claims
+                var userToCreate = new Entities.User();
+                userToCreate.Password = model.Password;
+                userToCreate.Username = model.Username;
+                userToCreate.IsActive = true;
+                userToCreate.Claims.Add(new Entities.UserClaim("country", model.Country));
+                userToCreate.Claims.Add(new Entities.UserClaim("address", model.Address));
+                userToCreate.Claims.Add(new Entities.UserClaim("given_name", model.Firstname));
+                userToCreate.Claims.Add(new Entities.UserClaim("family_name", model.Lastname));
+                userToCreate.Claims.Add(new Entities.UserClaim("email", model.Email));
+                userToCreate.Claims.Add(new Entities.UserClaim("subscriptionlevel", "FreeUser"));
+
+                // add it through the repository
+                _marvinUserRepository.AddUser(userToCreate);
+
+                if (!_marvinUserRepository.Save())
+                {
+                    throw new Exception($"Creating a user failed.");
+                }
+
+                // log the user in
+                //https://github.com/aspnet/Announcements/issues/232
+                //await HttpContext.Authentication.SignInAsync(userToCreate.SubjectId, userToCreate.Username);
+                await HttpContext.SignInAsync(userToCreate.SubjectId, userToCreate.Username);
+
+                // continue with the flow     
+                if (_interaction.IsValidReturnUrl(model.ReturnUrl) || Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+
+                return Redirect("~/");
+            }
+
+            // ModelState invalid, return the view with the passed-in model
+            // so changes can be made
+            return View(model);
+        }
+    }
+}
