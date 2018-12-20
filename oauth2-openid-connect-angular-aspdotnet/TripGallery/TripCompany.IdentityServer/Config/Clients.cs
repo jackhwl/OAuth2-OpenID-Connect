b1e3cd3476e7f2cbfb0000ae8abd8644ea5797b9
Jack Huang <jackhwl@hotmail.com>
2018-12-19 21:27:25 -0500

    Setting up Identity Server

---
+++ oauth2-openid-connect-angular-aspdotnet/TripGallery/TripCompany.IdentityServer/Config/Clients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+//using TripGallery;
+
+namespace TripCompany.IdentityServer.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        { 
+            return new List<Client>(); 
+        }
+    }
+}
---
+++ oauth2-openid-connect-angular-aspdotnet/TripGallery/TripCompany.IdentityServer/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using IdentityServer3.Core.Configuration;
+using Owin;
+using TripCompany.IdentityServer.Config;
+
+namespace TripCompany.IdentityServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/identity", idsrvApp =>
+            {
+                var idServerServiceFactory = new IdentityServerServiceFactory()
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+                    .UseInMemoryUsers(Users.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    Factory = idServerServiceFactory,
+                    SiteName = "TripCompany Security Token Service",
+                    IssuerUri = TripGallery.Constants.TripGalleryIssuerUri,
+                    PublicOrigin = TripGallery.Constants.TripGallerySTSOrigin,
+                    SigningCertificate = LoadCertificate()
+                };
+
+                idsrvApp.UseIdentityServer(options);
+            });  
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\certificates\idsrv3test.pfx",
+                    AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }    }
+}
